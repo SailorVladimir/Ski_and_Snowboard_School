149e8f0e98e0974bb0e8ba6e0bfb31ec9e0506e8
Anastasiya Pozyomina <snegirev.vladimir.sergeevich@gmail.com>
2020-02-18 16:34:16 +0700

    slider

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+    
+});
+
+
+
+
+// let ru = document.getElementById('slick-slide-control01').innerHTML = '';
+// let ru1 = document.getElementById('slick-slide-control00').innerHTML = '';
+// let ru3 = document.getElementById('slick-slide-control02').innerHTML = '';
+// let ru4 = document.querySelectorAll('#slick-slide-control01', ).innerHTML = '';
